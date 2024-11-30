7b79a234e6542bd4f5fccf34524ce43fb41b0810
tot121212 <totmusicbusiness@gmail.com>
2024-11-30 03:39:27 -0600

    finished 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let fToCEquation = (fahrenheit-32)*(5/9);
+  let oneDecimalPoint = 10;
+  let roundToOneDecimalPoint = Math.round(fToCEquation * oneDecimalPoint) / oneDecimalPoint;
+  return roundToOneDecimalPoint;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let cToFEquation =  celsius*(9/5)+32;
+  let oneDecimalPoint = 10;
+  let roundToOneDecimalPoint = Math.round(cToFEquation * oneDecimalPoint) / oneDecimalPoint;
+  return roundToOneDecimalPoint;
 };
 
 // Do not edit below this line
