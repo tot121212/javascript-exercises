96fb42d089ebc568ff1280918f278ebc33e0da16
tot121212 <totmusicbusiness@gmail.com>
2024-11-30 03:25:37 -0600

    finished 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year !== "number" || year < 0 || year % 1 != 0 || Number.isNaN(year)){
+        return "ERROR";
+    }
+    
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
