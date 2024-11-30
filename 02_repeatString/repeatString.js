b4b83da5cba013d1d58b063bf4db0e8fffface8b
tot121212 <totmusicbusiness@gmail.com>
2024-11-29 19:23:56 -0600

    finished 02_repeatStrings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(repeatString, repeatAmount) {
+    if (repeatAmount >= 0){
+        repeatedString = "";
+        for (i=0;i<repeatAmount;i++){
+            repeatedString += repeatString;
+        }
+        return repeatedString;
+    }
+    else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
