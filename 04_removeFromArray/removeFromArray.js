1eda36f577069a5bfaedc36401efe4dabbf47efc
tot121212 <totmusicbusiness@gmail.com>
2024-11-29 19:52:36 -0600

    finished 03_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removalArguments) {
+    return inputArray.filter((item) => !removalArguments.includes(item));
 };
 
 // Do not edit below this line
