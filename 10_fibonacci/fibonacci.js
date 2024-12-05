8fb814343ab0cdfe078c047e1ebdfbc3116f4b72
tot121212 <totmusicbusiness@gmail.com>
2024-12-05 03:40:30 -0600

    finished fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(arrLength){
+    // instead of creating a hashmap with a cache of solutions im just gonna do it the easy way for the sake of the exercise
+    arrLength = Number(arrLength);
+    if (arrLength === 0){
+        return 0;
+    }
+    
+    if (arrLength < 0 || typeof(arrLength) !== "number"){
+        return "OOPS";
+    }
+    const arr = new Array(); // create array of size arrLength
+    arr.unshift(1, 1); // prepend 1, 1 for fib sequence
+    for(i=2; i < arrLength; i++){ // iterate through up to length, skipping 2 first fib numbers
+        arr[i] = arr[i-2] + arr[i-1];
+    }
+    return arr[arrLength - 1];
 };
 
 // Do not edit below this line
