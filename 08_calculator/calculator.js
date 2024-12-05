c8e2d1bfbe01e84a8da51d39477a94dd67573e4c
tot121212 <totmusicbusiness@gmail.com>
2024-12-05 01:14:40 -0600

    finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args[0] + args[1];
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args[0] - args[1];
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	return args[0]
+  .reduce((total, number) => {
+    return total + Number(number);
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args[0]
+  .reduce((total, number) => {
+    return total * Number(number);
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(...args) {
+  const number = args[0];
+  const count = args[1];
+
+  const numbersArray = new Array(count).fill(number)
+
+  return numbersArray
+  .reduce((total, number) => {
+    return total * number;
+  }, 1);
+  // for count of arg[0], multiply by itself and return, arg[1] amount of times
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {  //multiply number by all positive integers less than it, down to 1
+  if (n < 0){
+    throw new Error("Cannot get factorial of negative numbers");
+  }
+  if (n === 0){
+    return 1; // base case
+  }
+
+  let result = 1;
+  for (let i = 1; i <= n; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
