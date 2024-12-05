76ee140e655064184b9b970c4a24ddf34fee7fea
tot121212 <totmusicbusiness@gmail.com>
2024-12-05 02:48:42 -0600

    finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    console.log(`str = ${str}`)
+   
+    const regex = /[^a-zA-Z0-9]/g;
+    const filteredStr = str.toLowerCase().replace(regex, ''); // take out all punctuation and word breaks
+    console.log(`filteredString = ${filteredStr}`);
+    
+    let left = 0;
+    let right = filteredStr.length - 1;
 
+    while (left < right){ // iterate over until no chars left to compare
+        if (filteredStr[left] !== filteredStr[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
