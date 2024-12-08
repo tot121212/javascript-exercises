70e7940dd294e55d36dfbf17fc3bc7ca3e047592
tot121212 <totmusicbusiness@gmail.com>
2024-12-08 10:48:56 -0600

    finished 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
+function reduceToOldestComparator(oldest, current) {
+    const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentYear - oldest.yearOfBirth;
+    const currentAge = current.yearOfDeath ? current.yearOfDeath - current.yearOfBirth : currentYear - current.yearOfBirth;
+    return currentAge > oldestAge ? current : oldest;
+}
 
-};
+const findTheOldest = function(people) {
+    return people.reduce(reduceToOldestComparator, people[0]); 
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
