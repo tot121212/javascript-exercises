88c69d628fc8f35b3de91f3becc7bafbe8ab1121
tot121212 <totmusicbusiness@gmail.com>
2024-12-09 23:01:02 -0600

    better solution to palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,22 +1,6 @@
-const palindromes = function(str) {
-    console.log(`str = ${str}`)
-   
-    const regex = /[^a-zA-Z0-9]/g;
-    const filteredStr = str.toLowerCase().replace(regex, ''); // take out all punctuation and word breaks
-    console.log(`filteredString = ${filteredStr}`);
-    
-    let left = 0;
-    let right = filteredStr.length - 1;
-
-    while (left < right){ // iterate over until no chars left to compare
-        if (filteredStr[left] !== filteredStr[right]){
-            return false;
-        }
-        left++;
-        right--;
-    }
-
-    return true;
+const palindromes = function(word) {
+    let cleanedWord = word.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    return cleanedWord === cleanedWord.split('').reverse().join('');
 };
 
 // Do not edit below this line
